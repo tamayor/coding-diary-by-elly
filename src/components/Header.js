2ed9cd66dbd54a7efaf5e06d7b2f9882d26169f7
yester-time <105924820+yester-time@users.noreply.github.com>
2023-08-09 15:26:26 +0800

    changint tagline

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header(){
     return(
         <header>
             <h1 className="font-size-50 text-color margin-5 text-center">Me Coding Diary</h1>
-            <p className="font-size-12 sub-text-color margin-5 text-center">Putting my codes in an e-Diary to enjoy coding in a simple way.</p>
+            <p className="font-size-12 sub-text-color margin-5 text-center"><i>Diary of Codes</i></p>
         </header>
     )
 }
